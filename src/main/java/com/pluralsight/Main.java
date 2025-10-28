196b6284ed18b6395ae323e038ccbb76fb2bac43
Ahsan <sbaseer25@gmail.com>
2025-10-28 14:44:43 -0400

    fixed the car class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -3,21 +3,46 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
         Vehicle vehicle = new Vehicle("red", 4, 4, 100);
-        System.out.println(vehicle);
+        vehicle.setTopSpeed(120);
+        vehicle.setAccelerationCapacity(10);
+        vehicle.setFuelConsumption(1);   // 1 unit per 10 miles
+        vehicle.setFuelLevel(10);        // start with 10 units
 
         Car car = new Car("black", 4, 2, 200);
-        System.out.println(car);
+        car.setFuelConsumption(2);       // 2 units per 10 miles (burns faster)
+        car.setFuelLevel(6);             // less fuel to force earlier refuels
 
-        vehicle.refuel(50);
         vehicle.start();
-        vehicle.accelerate();
-        System.out.println(vehicle);
-
-        car.refuel(25);
         car.start();
-        car.accelerate();
+
+        System.out.println("\nVEHICLE DRIVING");
+        int miles = 0;
+        while (miles < 100) {
+            if (vehicle.getFuelLevel() <= 0) {
+                System.out.println("Out of fuel → refuel +5");
+                vehicle.refuel(5);       // smaller refuel for Vehicle
+            }
+            miles += 10;
+            System.out.println("Traveled " + miles + " miles");
+            vehicle.accelerate();        // consumes fuel internally
+        }
+        vehicle.stop();
+
+        System.out.println("\nCAR DRIVING");
+        int carMiles = 0;
+        while (carMiles < 100) {
+            if (car.getFuelLevel() <= 0) {
+                System.out.println("Out of fuel → refuel +8");
+                car.refuel(8);           // bigger refuel for Car
+            }
+            carMiles += 10;
+            System.out.println("Traveled " + carMiles + " miles");
+            car.accelerate();            // consumes more fuel than Vehicle
+        }
+        car.stop();
+
+        System.out.println("\nAFTER TRIP");
+        System.out.println(vehicle);
         System.out.println(car);
-
-
     }
 }
