4fa1f031bac7fc5947b85584c97fb3effbb8cce5
Ahsan <sbaseer25@gmail.com>
2025-10-28 20:38:34 -0400

    added tostring override to car

--- src/main/java/com/pluralsight/SpaceShip.java
+++ src/main/java/com/pluralsight/SpaceShip.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
-public class SpaceShip {
+public class SpaceShip extends Vehicle {
+    public SpaceShip(String color, int numOfPassenger, int cargoCapacity, int fuelCapacity) {
+        super(color, numOfPassenger, cargoCapacity, fuelCapacity);
+    }
+
+    @Override
+    public String toString() {
+        return "Spaceship{" +
+                "color='" + getColor() + '\'' +
+                ", numOfPassenger=" + getNumOfPassenger() +
+                ", cargoCapacity=" + getCargoCapacity() +
+                ", fuelCapacity=" + getFuelCapacity() +
+                ", topSpeed=" + getTopSpeed() +
+                ", speed=" + getSpeed() +
+                ", accelerationCapacity=" + getAccelerationCapacity() +
+                ", fuelLevel=" + getFuelLevel() +
+                ", fuelConsumption=" + getFuelConsumption() +
+                '}';
+    }
 }
